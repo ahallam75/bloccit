228e319c28015bb2a87368f5363ddb9e744dac8d
ahallam <alexhallam75@gmail.com>
2018-09-28 11:55:45 -0400

    Assignment submitted

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = 'http://localhost:3000/marco';
 
 describe("routes : static", () => {
 
@@ -19,5 +20,25 @@ describe("routes : static", () => {
       });
     });
 
+//#1
+
+    describe("GET /marco", () => {    
+
+//#2
+      it("should return status code 200 and have \'polo\' in the body", (done) => {
+
+//#3
+        request.get(marco, (err, res, body) => {
+          expect(res.statusCode).toBe(200);
+          expect(body).toBe("polo");
+
+//#4
+          done();
+          });
+      });
+
+    });
+
   });
+
 });
