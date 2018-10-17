d6d0c9f18433da76b4f64c0838598ad5c1049c49
ahallam <alexhallam75@gmail.com>
2018-10-17 09:56:43 -0400

    First commit

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -19,7 +19,6 @@ module.exports = {
     validateUsers(req, res, next) {
       if(req.method === "POST") {
  
- // #1
         req.checkBody("email", "must be valid").isEmail();
         req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
         req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
@@ -32,6 +31,21 @@ module.exports = {
         return res.redirect(req.headers.referer);
       } else {
         return next();
+      }
+    },
+
+    validateComments(req, res, next) {
+      if(req.method === "POST") {
+        req.checkBody("body", "must not be empty"). notEmpty();
+      }
+ 
+      const errors = req.validationErrors();
+ 
+      if (errors) {
+        req.flash("error", errors);
+        return res.redirect(req.headers.referer);
+      } else {
+        return next()
       }
     }
   }
