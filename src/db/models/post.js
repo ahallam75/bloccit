60c9c9ceff02e31be00ee49f585aa8daed28e238
ahallam <alexhallam75@gmail.com>
2018-10-29 13:12:10 -0400

    First commit: work complete

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -61,9 +61,16 @@ module.exports = (sequelize, DataTypes) => {
           .reduce((prev, next) => { return prev + next });
   };
   Post.prototype.getFavoriteFor = function(userId){
-    return this.favorites.find((favorite) => { return favorite.userId == userId });
+    return this.favorites.find((favorite) => { 
+      return favorite.userId == userId });
   };
-
+  Post.addScope("lastFiveFor", (userId) => {
+    return {
+      where: { userId: userId},
+      limit: 5,
+      order: [["createdAt", "DESC"]]
+    }
+  });
    
   return Post;
 };
